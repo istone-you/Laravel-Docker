1101e9bbf6b10f25b4a308305de0f2ed3f585cb5
EC2 Default User <ec2-user@ip-172-31-43-9.ap-northeast-1.compute.internal>
2022-04-18 06:33:13 +0000

    dockerdockerの設定（途中経過）

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +11,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WebController@index');
+
+Route::get('users/carts', 'CartController@index')->name('carts.index');
+Route::post('users/carts', 'CartController@store')->name('carts.store');
+Route::delete('users/carts', 'CartController@destroy')->name('carts.destroy');
+Route::get('users/mypage', 'UserController@mypage')->name('mypage');
+Route::get('users/mypage/edit', 'UserController@edit')->name('mypage.edit');
+Route::get('users/mypage/address/edit', 'UserController@edit_address')->name('mypage.edit_address');
+Route::put('users/mypage', 'UserController@update')->name('mypage.update');
+Route::get('users/mypage/favorite', 'UserController@favorite')->name('mypage.favorite');
+Route::get('users/mypage/password/edit', 'UserController@edit_password')->name('mypage.edit_password');
+Route::put('users/mypage/password', 'UserController@update_password')->name('mypage.update_password');
+
+Route::post('products/{product}/reviews', 'ReviewController@store');
+Route::get('products/{product}/favorite', 'ProductController@favorite')->name('products.favorite');
+Route::resource('products', 'ProductController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
